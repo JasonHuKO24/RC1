432a8b84fcf0d141b62c3e1da5055210b928e261
JaosnHuKO24 <164616986@qq.com>
2019-03-16 09:49:13 +0800

    day3

--- day1.1/demo.js
+++
@@ -1,7 +0,0 @@
-var http = require ('http')
-var server = http.createServer(function(req,res){
-
-    res.writeHead(200,{"content-type":"text/html;charset=utf-8"})
-    res.end("hahaha")
-})
-server.listen(8088,"127.0.0.1")
